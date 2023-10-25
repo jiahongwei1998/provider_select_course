13ccf83e64a74cedef7d48c43359385a526f4773
David Jia <105570809@qq.com>
2023-10-25 18:44:50 +0800

    first commit

---
+++ android/app/src/main/kotlin/dev/hjia/provider_select_coutse/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.hjia.provider_select_coutse
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      useMaterial3: true,
+    ),
+    home: const HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+@immutable
+class BaseObject {
+  final String id;
+  final String lastUpdated;
+
+  BaseObject()
+      : id = const Uuid().v4(),
+        lastUpdated = DateTime.now().toIso8601String();
+
+  @override
+  bool operator ==(covariant BaseObject other) => id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
+}
+
+class ExpensiveObject extends BaseObject {}
+
+class CheapObject extends BaseObject {}
+
+class ObjectProvider extends ChangeNotifier {}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Home Page'),
+      ),
+    );
+  }
+}
